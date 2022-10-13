043d8246566cb6123c27f09cd89f27b86b815d26
IrohasuAndRedbull <114926323+IrohasuAndRedbull@users.noreply.github.com>
2022-10-13 14:03:23 +0900

    Create Rei0101.php

---
+++ Lesson02/Rei/Rei0101.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>   
+<meta charset="UTF-8">
+<title>PHPの基本</title>
+</head>
+<body>
+<?php
+  $hankei = 5;
+  $ritu = 3.14;
+  
+  $menseki = $ritu$hankei;
+  
+  echo "PHPプログラムの練習";
+  echo "半径 $hankei の、円の面積は $menseki です。";
+?>
+</body>
+</html>
