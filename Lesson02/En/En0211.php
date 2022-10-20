de5d7c29d8c0367be8ae3923ea8aec5dcc0aa4bb
IrohasuAndRedbull <114926323+IrohasuAndRedbull@users.noreply.github.com>
2022-10-20 09:51:24 +0900

    Update En0211.php

--- Lesson02/En/En0211.php
+++ Lesson02/En/En0211.php
@@ -14,21 +14,20 @@
     $lobp=$_POST["lobp"];               /*最低血圧*/
     $fbs=$_POST["fbs"];                 /*空腹時血糖値*/
     
-    $count=0;
+    $count01=0;
+    $count02=0;
 
 if($sex==0){
-    if($waistcircumference>=85) $count++;
+    if($waistcircumference>=85) $count01++;
 }else{
-    if($waistcircumference>=90) $count++;
+    if($waistcircumference>=90) $count01++;
 }
 
-    if($neutralfat>=150) $count++;
-    if($hdlkoru<40) $count++;
-    if($hobp>=130) $count++;
-    if($lobp>=85) $count++;
-    if($fbs>=110) $count++;
+    if($neutralfat>=150 || $hdlkoru<40) $count02++;
+    if($hobp>=130 || $lobp>=85) $count02++;
+    if($fbs>=110) $count02++;
 
-if($count>0) echo "あなたはメタボリックシンドロームの可能性があります。";
+if($count01==1 && $count02>=2) echo "あなたはメタボリックシンドロームの可能性があります。";
 else echo "あなたはメタボリックシンドロームの可能性はありません。"
 ?>
 </body>
